cb3d423156ada6ddb46ae2d49baea1f9df7c727f
SaidRem <said.r@yandex.ru>
2025-06-18 22:13:32 +0400

    Created ORM

---
+++ phone_catalog/phones/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import index, show_catalog, show_product
+
+urlpatterns = [
+    path('', index, name="index"),
+    path('catalog/', show_catalog, name='catalog'),
+    path('catalog/<slug:slug>/', show_product, name='product')
+]
