f342b562393a7c1b624d73b344a32323085a42d4
SaidRem <said.r@yandex.ru>
2025-06-19 12:19:33 +0400

    Settings for postgres database

--- phone_catalog/phone_catalog/settings.py
+++ phone_catalog/phone_catalog/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'phone_catalog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'phone_catalog',
+        'USER': 'postgres',
+        'PASSWORD': '123',
     }
 }
 
