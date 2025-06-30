89359910d338679e696cc36112ed1b06dacd1282
SaidRem <said.r@yandex.ru>
2025-06-30 16:45:15 +0400

    Updated student_list view with prefetch_related

--- school_website/school/views.py
+++ school_website/school/views.py
@@ -1,12 +1,11 @@
-from django.views.generic import ListView
 from django.shortcuts import render
-
+from django.db.models import Prefetch
 from .models import Student
 
 
 def students_list(request):
     template = 'school/students_list.html'
-    students = Student.objects.order_by('group')
+    students = Student.objects.prefetch_related('teachers').order_by('group')
     context = {
         'students': students
     }
