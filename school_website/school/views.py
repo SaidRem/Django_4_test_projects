51119cd73b99029f8dbd6457f641d25f90bb0c21
SaidRem <said.r@yandex.ru>
2025-06-30 12:08:48 +0400

    Added student_list view

---
+++ school_website/school/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+from django.shortcuts import render
+
+from .models import Student
+
+
+def students_list(request):
+    template = 'school/students_list.html'
+    students = Student.objects.order_by('group')
+    context = {
+        'students': students
+    }
+
+    return render(request, template, context)
