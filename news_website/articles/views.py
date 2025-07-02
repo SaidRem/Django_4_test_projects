7433c7084f2c19a5d2369a66396e0988bc7340ed
SaidRem <said.r@yandex.ru>
2025-07-02 15:16:18 +0400

    view articles_list

--- news_website/articles/views.py
+++ news_website/articles/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Article
 
-# Create your views here.
+
+def articles_list(request):
+    template = 'articles/articles_list.html'
+
+    articles = Article.objects.prefetch_related('scopes__tag')
+
+    for article in articles:
+        article.sorted_scopes = sorted(
+            article.scopes.all(),
+            key=lambda s: (not s.is_main, s.tag.name)
+        )
+
+    context = {'articles': articles}
+    return render(request, template, context)
