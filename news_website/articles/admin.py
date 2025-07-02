4de630a6066ca91f560291ebb803534a4a0606a9
SaidRem <said.r@yandex.ru>
2025-07-02 18:22:23 +0400

    models admin

--- news_website/articles/admin.py
+++ news_website/articles/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-# Register your models here.
+from .models import Article, Scope, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tag_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main', False):
+                main_tag_count += 1
+
+            if main_tag_count > 1:
+                raise ValidationError('Только один тег может быть основным.')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Scope
+    formset = RelationshipInlineFormset
+    extra = 1
+
+
+@admin.register(Article)
+class ObjectAdmin(admin.ModelAdmin):
+    inlines = [RelationshipInline]
+    list_display = ['title', 'published_at', 'tag_count']
+    search_fields = ['title', 'text']
+
+    def tag_count(self, obj):
+        return obj.scopes.count()
+    tag_count.shor_description = 'Number of Tags'
